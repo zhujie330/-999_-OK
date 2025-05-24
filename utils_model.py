0ca8fd8dc04239525a83cea0ecf25214eb4ca5b6
zhujie330 <3068563162@qq.com>
2025-05-24 12:38:54 +0800

    Update utils_model.py

--- utils_model.py
+++ utils_model.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 import streamlit as st
 from modelscope.hub.snapshot_download import snapshot_download
-st.write("⚠️ 由于 Git LFS 流量已达上线，自动转从 ModelScope 联网加载模型，")
+
 @st.cache_resource(show_spinner="加载模型中，请稍候...")
 def get_model_dir():
     model_dir = os.path.join(tempfile.gettempdir(), 'model_use414')
