c191cc0e257c01b62523077282b0e76282a688c5
zhujie330 <3068563162@qq.com>
2025-05-24 12:25:20 +0800

    Create utils_model.py

---
+++ utils_model.py
@@ -0,0 +1,13 @@
+import os
+import tempfile
+import streamlit as st
+from modelscope.hub.snapshot_download import snapshot_download
+
+@st.cache_resource(show_spinner="加载模型中，请稍候...")
+def get_model_dir():
+    model_dir = os.path.join(tempfile.gettempdir(), 'model_use414')
+    model_file_path = os.path.join(model_dir, 'model1.pth')
+
+    if not os.path.exists(model_file_path):
+        model_dir = snapshot_download('zhujie67o/model_use414')
+    return model_dir
