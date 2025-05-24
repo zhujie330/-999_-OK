e39e0c47a48345131aa32f29ed3d82152a549a01
zhujie330 <3068563162@qq.com>
2025-05-24 12:51:14 +0800

    Update 2_Deepfake_Analysis.py

--- pages/2_Deepfake_Analysis.py
+++ pages/2_Deepfake_Analysis.py
@@ -22,13 +22,14 @@ from utils_model import get_model_dir
 st.set_page_config(page_title="Deepfake Detection", page_icon="🔬")
 st.sidebar.header("🔬Deepfake Detection")
 st.write("# Demo for Deepfake Analysis🔬")
+st.write("⚠️ 由于 Git LFS 流量已达上线，自动转从 ModelScope 联网加载模型，请稍后")
 
 model_dir = get_model_dir()
 model_file_path = os.path.join(model_dir, 'model1.pth')
 if os.path.exists(model_file_path):
     st.write("✔️ 模型已加载")
 else:
-    st.write("⚠️ 由于 Git LFS 流量已达上线，自动转从 ModelScope 联网加载模型，请稍后")
+    st.write("⚠️ 模型文件未找到，请稍候重试")
 
 
     st.write("✔️ 模型已加载")
