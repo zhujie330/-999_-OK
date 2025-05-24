3e0bded691552de54505230bd3f6408a6cbaae77
zhujie330 <3068563162@qq.com>
2025-05-24 12:42:35 +0800

    Update utils_model.py

--- utils_model.py
+++ utils_model.py
@@ -8,6 +8,7 @@ def get_model_dir():
     model_dir = os.path.join(tempfile.gettempdir(), 'model_use414')
     model_file_path = os.path.join(model_dir, 'model1.pth')
 
-    if not os.path.exists(model_file_path):
+    is_first_download = not os.path.exists(model_file_path)
+    if is_first_download:
         model_dir = snapshot_download('zhujie67o/model_use414')
-    return model_dir
+    return model_dir, is_first_download
